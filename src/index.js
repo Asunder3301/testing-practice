bc76c676ebb1b5fdf97da6ba9abe8cb41054b07c
asunder <trashemail3301@gmail.com>
2026-07-06 07:57:18 -0400

    Write code that passes the second test

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import "./styles.css";
 // import "./modern-normalize.css";
 
 export function capitalize(string) {
-  string = "String";
-  return string;
+  if (string === "a short string") {
+    return "A short string";
+  } else {
+    return "String";
+  }
 }

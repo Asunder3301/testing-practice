72c1b158c8362977ef2c5f48d169bba503c5b3fa
asunder <trashemail3301@gmail.com>
2026-07-06 07:55:41 -0400

    Write another test that checks for capitalization

--- src/test/capitalize.test.js
+++ src/test/capitalize.test.js
@@ -1,5 +1,9 @@
 import { capitalize } from "../index";
 
 test("Capitalize first letter", () => {
-    expect(capitalize("string")).toBe("String");
+  expect(capitalize("string")).toBe("String");
+});
+
+test("Capitalize first letter", () => {
+  expect(capitalize("a short string")).toBe("A short string");
 });

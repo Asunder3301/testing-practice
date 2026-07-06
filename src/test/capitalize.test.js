076ec816506a63960e2f3152561d6433080f4011
asunder <trashemail3301@gmail.com>
2026-07-06 08:16:19 -0400

    refactor test code

--- src/test/capitalize.test.js
+++ src/test/capitalize.test.js
@@ -1,9 +1,18 @@
 import { capitalize } from "../index";
 
-test("Capitalize first letter", () => {
-  expect(capitalize("string")).toBe("String");
-});
+test("Capitalize the first letter of a given string", () => {
+  const testCases = [
+    {
+      input: "string",
+      expected: "String",
+    },
+    {
+      input: "a short string",
+      expected: "A short string",
+    },
+  ];
 
-test("Capitalize first letter", () => {
-  expect(capitalize("a short string")).toBe("A short string");
+  testCases.forEach((test) => {
+    expect(capitalize(test.input)).toBe(test.expected);
+  });
 });

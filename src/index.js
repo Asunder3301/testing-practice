f4cbaabe9c73eee308992272edbb154582d19e6c
asunder <trashemail3301@gmail.com>
2026-07-06 08:20:52 -0400

    Refactor capitalize function

--- src/index.js
+++ src/index.js
@@ -2,9 +2,5 @@ import "./styles.css";
 // import "./modern-normalize.css";
 
 export function capitalize(string) {
-  if (string === "a short string") {
-    return "A short string";
-  } else {
-    return "String";
-  }
+  return `${string.charAt(0).toUpperCase()}${string.slice(1)}`;
 }

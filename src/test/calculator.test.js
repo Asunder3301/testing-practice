a77ed462b2b8f2181952e129c83cfa7539363931
asunder <trashemail3301@gmail.com>
2026-07-06 09:38:41 -0400

    Add more test cases for each arithmetic function

--- src/test/calculator.test.js
+++ src/test/calculator.test.js
@@ -1,17 +1,60 @@
 import { calculator } from "../index.js";
 
+const testCases = [
+  {
+    a: 1,
+    b: 1,
+    add: 2,
+    subtract: 0,
+    multiply: 1,
+    divide: 1,
+  },
+  {
+    a: 2,
+    b: 2,
+    add: 4,
+    subtract: 0,
+    multiply: 4,
+    divide: 1,
+  },
+  {
+    a: 5,
+    b: 10,
+    add: 15,
+    subtract: -5,
+    multiply: 50,
+    divide: 0.5,
+  },
+  {
+    a: 100,
+    b: 5,
+    add: 105,
+    subtract: 95,
+    multiply: 500,
+    divide: 20,
+  },
+];
+
 test("Addition", () => {
-  expect(calculator.add(1, 1)).toBe(2);
+  testCases.forEach((test) => {
+    expect(calculator.add(test.a, test.b)).toBe(test.add);
+  });
 });
 
 test("Subtraction", () => {
-  expect(calculator.subtract(2, 1)).toBe(1);
+  testCases.forEach((test) => {
+    expect(calculator.subtract(test.a, test.b)).toBe(test.subtract);
+  });
 });
 
 test("Multiplication", () => {
-  expect(calculator.multiply(2, 2)).toBe(4);
+  testCases.forEach((test) => {
+    expect(calculator.multiply(test.a, test.b)).toBe(test.multiply);
+  });
 });
 
 test("Division", () => {
-  expect(calculator.divide(2, 2)).toBe(1);
+  testCases.forEach((test) => {
+    expect(calculator.divide(test.a, test.b)).toBe(test.divide);
+  });
 });

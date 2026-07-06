7c29f7bb83a6e70b24fdc9c9b7a52ccfe7f896b4
asunder <trashemail3301@gmail.com>
2026-07-06 09:51:27 -0400

    Create object that passes the tests

--- src/index.js
+++ src/index.js
@@ -9,4 +9,19 @@ function reverseString(string) {
   return string.split("").reverse().join("");
 }
 
-export { capitalize, reverseString };
+const calculator = {
+  add: (a, b) => {
+    return a + b;
+  },
+  subtract: (a, b) => {
+    return a - b;
+  },
+  multiply: (a, b) => {
+    return a * b;
+  },
+  divide: (a, b) => {
+    return a / b;
+  },
+};
+
+export { capitalize, reverseString, calculator };

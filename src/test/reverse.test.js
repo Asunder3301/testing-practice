c417bff6dbcab447a4c3f863612d62727fbc579c
asunder <trashemail3301@gmail.com>
2026-07-06 08:34:23 -0400

    Write a second test for revereString function

--- src/test/reverse.test.js
+++ src/test/reverse.test.js
@@ -3,3 +3,7 @@ import { reverseString } from "../index";
 test("Reverse a given string", () => {
   expect(reverseString("string")).toBe("gnirts");
 });
+
+test("Reverse a given string (Case 2)", () => {
+  expect(reverseString("a short string")).toBe("gnirts trohs a");
+});
